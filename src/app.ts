a2570b8c291a8e246cb084a765db3326f61bb216
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-09-03 20:09:49 +0200

    upload app ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import "express-async-errors";
 import express from "express";
 import cors from "cors";
-
+import uploadConfig from "./configs/upload";
 import { routes } from "./routes";
 import { errorHandling } from "./middlewares/error-handling";
 
@@ -9,6 +9,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(routes);
 app.use(errorHandling);
 
